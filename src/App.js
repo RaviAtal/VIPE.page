35cfb1fe0851f58bf8ff7ba6b8cbbcc09c0b2d3b
RaviAtal <raviatal44@gmail.com>
2023-03-29 22:56:27 +0530

    Integer_section

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import Header from "./Components/Header";
+import FeaturedOn from "./Components/FeaturedOn";
+import Integer from "./Components/Integer";
 
 function App() {
   return (
     <>
       <Header />
+      <FeaturedOn />
+      <Integer />
     </>
   );
 }
---
+++ src/Components/Integer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Integer() {
+  return (
+    <>
+      <section>
+        <div className="container py-4 py-sm-5">
+          <h2 className="mb-0 ff_kon fw_400 fs_2xl">Integer et nisl non</h2>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Integer;
